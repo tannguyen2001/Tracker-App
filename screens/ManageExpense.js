b7132546fb20171ea37f78b71638de7e5452698c
Tan Nguyen <khodo3136@gmail.com>
2022-08-29 15:00:31 +0700

    style ManageExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
+
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -11,7 +15,64 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditting]);
 
-  return <Text>All ManageExpense screen</Text>;
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditting ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditting && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
 
 export default ManageExpense;
